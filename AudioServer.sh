e6943a37e7957e5bcc58d61550be2bddc7dd3573
Delmat237 <azangueleonel9@gmail.com>
2025-02-02 00:06:17 +0100

    scripts bash for running server and chatApp

---
+++ AudioServer.sh
@@ -0,0 +1,1 @@
+java --module-path /home/delmat/ENSPY/ALAANYA_COM-main/lib/libJavaFx --add-modules javafx.controls,javafx.fxml -cp bin:/home/delmat/ENSPY/ALAANYA_COM-main/lib/mysql-connector-j-8.3.0.jar alaanya.AudioRelayServer
