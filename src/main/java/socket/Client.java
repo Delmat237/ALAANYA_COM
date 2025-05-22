0e1cb895f63042e78f6a77577898b6048d32b62a
Charlinebrenda <tchaweuc@gmail.github>
2025-05-22 11:18:04 +0200

    style frontend light theme new update

--- src/main/java/socket/Client.java
+++ src/main/java/socket/Client.java
@@ -27,7 +27,7 @@ public class Client {
             .setDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSZ")
             .create();
 
-    private static final String CENTRAL_SERVER_IP = "127.0.0.1";
+    private static final String CENTRAL_SERVER_IP = "10.2.64.14";
     private static final int CENTRAL_SERVER_PORT = 7000;
 
     public interface NotificationListener {
